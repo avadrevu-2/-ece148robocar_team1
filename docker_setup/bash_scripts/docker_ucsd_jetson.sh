9bc0f005c253767126d860b968f6ce65d94dabfd
Dominic Nightingale <djnighti@ucsd.edu>
2022-06-17 23:06:37 +0000

    Update docker_ucsd_jetson.sh

--- docker_setup/bash_scripts/docker_ucsd_jetson.sh
+++ docker_setup/bash_scripts/docker_ucsd_jetson.sh
@@ -1,9 +1,9 @@
 docker run \
-    --name name_this_container \
+    --name ${1:-test} \
     -it \
     --privileged \
     --net=host \
     -e DISPLAY=$DISPLAY \
     --device /dev/video0 \
     --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    djnighti/ucsd_robocar:latest
+    djnighti/ucsd_robocar:${2:-latest}
