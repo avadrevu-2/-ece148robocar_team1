2999fda97d894966e87981c36d7df21cdd462825
Dominic Nightingale <djnighti@ucsd.edu>
2022-07-10 02:18:08 +0000

    Revert "Update docker_ucsd_jetson.sh"
    This reverts commit 9bc0f005c253767126d860b968f6ce65d94dabfd

--- docker_setup/bash_scripts/docker_ucsd_jetson.sh
+++ docker_setup/bash_scripts/docker_ucsd_jetson.sh
@@ -1,9 +1,9 @@
 docker run \
-    --name ${1:-test} \
+    --name name_this_container \
     -it \
     --privileged \
     --net=host \
     -e DISPLAY=$DISPLAY \
     --device /dev/video0 \
     --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    djnighti/ucsd_robocar:${2:-latest}
+    djnighti/ucsd_robocar:latest
